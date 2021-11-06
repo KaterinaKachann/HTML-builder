2ba3fe00e514c6d3f2d75b4930110107c228292f
Ekaterina Kachan <eka.kachan@gmail.com>
2021-11-07 00:29:50 +0300

    fix: fix solution in task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,22 +3,29 @@ const path = require("path");
 const filesCopyPath = path.join(__dirname, "files-copy");
 const filesPath = path.join(__dirname, "files");
 
-// create new directory files-copy
-fs.mkdir(filesCopyPath, { recursive: true }, (error) => {
-  if (error) {
-    throw error;
-  }
+
+fs.rm(filesCopyPath, { recursive: true }, function () {
+  fs.mkdir(filesCopyPath, { recursive: true }, (error) => {
+    if (error) {
+      throw error;
+    }
+
+    fs.readdir(filesPath, { withFileTypes: true }, function (err, files) {
+      //   listing all files using forEach
+      files.forEach(function (file) {
+        
+        fs.copyFile(
+          filesPath + "/" + file.name,
+          filesCopyPath + "/" + file.name,
+          function () {}
+        );
+    
+        
+      });
+    });
+  });
+
 });
 
-fs.readdir(filesPath, { withFileTypes: true }, function (err, files) {
-//   listing all files using forEach
-  files.forEach(function (file) {
-    console.log(file);
-    fs.copyFile(
-        filesPath + "/" + file.name, 
-        filesCopyPath + "/" +file.name,
-        function() {}
-    );
-  });
-});
+
 
